35d398cd5c7dfe903e3e5348302dbdf0e6450361
Riccardo Sebastianelli <riccardinoseba01@gmail.com>
2023-05-16 00:35:35 +0200

    Finita form artista

--- src/main/java/it/uniroma3/siw/repository/ArtistRepository.java
+++ src/main/java/it/uniroma3/siw/repository/ArtistRepository.java
@@ -1,11 +1,15 @@
 package it.uniroma3.siw.repository;
 
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import it.uniroma3.siw.model.Artist;
+import it.uniroma3.siw.model.Movie;
 
 public interface ArtistRepository extends CrudRepository<Artist, Long> {
 
@@ -19,5 +23,10 @@ public interface ArtistRepository extends CrudRepository<Artist, Long> {
 			+ "where movie_actors.starred_movies_id = :movieId)", nativeQuery=true)
 	public Iterable<Artist> findActorsNotInMovie(@Param("movieId") Long id);
 
+	@Query("SELECT m FROM Movie m JOIN FETCH m.director d WHERE d.id = :directorId ORDER BY m.rating DESC")
+	List<Movie> findTopDirectorMoviesOrderByRatingDesc(Pageable pageable, @Param("directorId") Long id);
+
+	@Query("SELECT m FROM Movie m JOIN m.actors a WHERE a.id = :actorId ORDER BY m.rating DESC")
+List<Movie> findTopActorMoviesOrderByRatingDesc(Pageable pageable, @Param("actorId") Long id);
 
 }
