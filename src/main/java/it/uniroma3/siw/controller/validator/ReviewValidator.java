161afd0a32a3be6966849a290bd634715c01f620
Riccardo Sebastianelli <riccardinoseba01@gmail.com>
2023-05-20 12:28:33 +0200

    Aggiunta gestione recensioni

---
+++ src/main/java/it/uniroma3/siw/controller/validator/ReviewValidator.java
@@ -0,0 +1,28 @@
+package it.uniroma3.siw.controller.validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import it.uniroma3.siw.model.Movie;
+import it.uniroma3.siw.model.Review;
+import it.uniroma3.siw.repository.ReviewRepository;
+
+@Component
+public class ReviewValidator implements Validator {
+	@Autowired
+	private ReviewRepository reviewRepository;
+
+	@Override
+	public void validate(Object o, Errors errors) {
+		Review review = (Review)o;
+		if (review.getTitle()!=null && review.getReviewDescription()!=null && review.getMovie()!=null && review.getUser()!=null 
+				&& reviewRepository.existsByMovieIdAndUserId(review.getMovie().getId(), review.getUser().getId() )) {
+			errors.reject("review.duplicate");
+		}
+	}
+	@Override
+	public boolean supports(Class<?> aClass) {
+		return Review.class.equals(aClass);
+	}
+}
